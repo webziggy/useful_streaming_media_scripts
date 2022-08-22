08b9556c24273e8bd33cea20acc4c87c04b1f60c
webziggy <alan.ogilvie@gmail.com>
2022-08-22 09:56:24 +0100

    Update ffmpeg_brew_update.sh
    libzvbi isn't installed automatically now, with DVB functionality in FFMPEG. Corrected script to install it as per advice on https://github.com/homebrew-ffmpeg/homebrew-ffmpeg
    
    Also removed the decklink dependency - if you need it, read through the instructions at the link above (I've not found a need to have it working)

--- ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
+++ ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-#
-# This script, for macOS, tries to install ffmpeg with most of the options in the right order.
-#
 brew uninstall --force --ignore-dependencies ffmpeg
-brew install chromaprint amiaopensource/amiaos/decklinksdk
+brew uninstall --force chromaprint amiaopensource/amiaos/decklinksdk
+#brew install chromaprint amiaopensource/amiaos/decklinksdk
+brew uninstall zvbi
+brew tap lescanauxdiscrets/tap && brew install lescanauxdiscrets/tap/zvbi
+brew install chromaprint
+brew uninstall --force --ignore-dependencies ffmpeg
 brew tap homebrew-ffmpeg/ffmpeg
-brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi game-music-emu | tr '\n' ' ')
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi game-music-emu | grep -vi decklink | tr '\n' ' ')

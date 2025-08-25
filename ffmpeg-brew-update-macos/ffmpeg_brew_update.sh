7f47fa7e2b80678bf901803006bf047810c57644
Alan Ogilvie <alan.ogilvie@gmail.com>
2025-08-25 22:33:44 +0100

    removed 'whisper' as it was crashing configuration of ffmpeg on macos.
    removed 'whisper' as it was crashing configuration of ffmpeg on macos.

--- ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
+++ ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
@@ -6,4 +6,4 @@ brew tap lescanauxdiscrets/tap && brew install lescanauxdiscrets/tap/zvbi
 brew install chromaprint
 brew uninstall --force --ignore-dependencies ffmpeg
 brew tap homebrew-ffmpeg/ffmpeg
-brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi game-music-emu | grep -vi decklink | grep -vi librsvg | grep -vi libflite | tr '\n' ' ')
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi game-music-emu | grep -vi decklink | grep -vi openvino | grep -vi whisper | grep -vi librsvg | grep -vi libflite | tr '\n' ' ')

56df247adca735103bef98e409fe52594cc25b4d
Alan Ogilvie <alan.ogilvie@gmail.com>
2026-05-05 15:21:57 +0100

    Document and adjust ffmpeg brew install script
    Add explanatory comments about Decklink (manual SDK install required) and tidy the install flow: force-uninstall ffmpeg, reinstall dependencies (chromaprint, zvbi) and tap required repos. Comment out decklinksdk installation, add environment-variable hints for Decklink builds, and update the final homebrew-ffmpeg install command to explicitly filter out decklink and other unsupported options. Also reorder and clarify steps with inline notes.
    Also FFMPEG moved to using the alt-name for the binary installed and this wasn't weeding that out of the options list, so this updated stops it from installing the binary with the alt-name (this shell script removes '--with-alt-name' from the options when installing ffmpeg so it should be installed with the default names 'ffmpeg', 'ffplay').

--- ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
+++ ffmpeg-brew-update-macos/ffmpeg_brew_update.sh
@@ -1,9 +1,25 @@
+# Decklink requires you to download and install decklinkSDK from Blackmagic as it just doesn't work, but even then - I can't get it to work.
+
+#tidying up by forcefully uninstalling stuff…
 brew uninstall --force --ignore-dependencies ffmpeg
 brew uninstall --force chromaprint amiaopensource/amiaos/decklinksdk
-#brew install chromaprint amiaopensource/amiaos/decklinksdk
 brew uninstall zvbi
+#Do some installs for dependencies…
+brew install chromaprint 
+#Since I can't get decklinksdk to include properly, I've commented this line out…
+#brew install amiaopensource/amiaos/decklinksdk
 brew tap lescanauxdiscrets/tap && brew install lescanauxdiscrets/tap/zvbi
+#Oddly I have a double attempt to install chromaprint, just in case…
 brew install chromaprint
+#Now forcefully uninstall ffmpeg but ignore dependencies (so it doesn't uninstall things we need later)…
 brew uninstall --force --ignore-dependencies ffmpeg
+#Tap the ffmpeg in case we haven't got that already…
 brew tap homebrew-ffmpeg/ffmpeg
-brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi with-alt-name | grep -vi chromaprint | grep -vi game-music-emu | grep -vi decklink | grep -vi openvino | grep -vi whisper | grep -vi librsvg | grep -vi libflite | tr '\n' ' ')
+
+#For Decklink, we apparently need these two environment variables, and then the decklink install is on line 22 - but line 24 is currently our main FFMPEG install without decklink support.
+#HOMEBREW_EXTRA_CFLAGS="-I$HOME/Documents/Blackmagic-DeckLink-SDK-16.0/Mac/include"
+#HOMEBREW_EXTRA_LDFLAGS="-L$HOME/Documents/Blackmagic-DeckLink-SDK-16.0/Mac/include"
+#brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi alt-name | grep -vi game-music-emu | grep -vi openvino | grep -vi whisper | grep -vi librsvg | grep -vi libflite | tr '\n' ' ')
+
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg $(brew options homebrew-ffmpeg/ffmpeg/ffmpeg | grep -vE '\s' | grep -- '--with-' | grep -vi chromaprint | grep -vi alt-name | grep -vi game-music-emu | grep -vi decklink | grep -vi openvino | grep -vi whisper | grep -vi librsvg | grep -vi libflite | tr '\n' ' ')
+
